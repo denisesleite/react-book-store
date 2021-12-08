d5c3b7f64869071d9b2d0f052efb575e0894967c
denisesleite <denisegd01@gmail.com>
2021-12-08 16:43:55 -0300

    mudanças na estrutura

--- front/src/components/Card/Card.styles.ts
+++ front/src/components/Card/Card.styles.ts
@@ -3,7 +3,6 @@ import { Box } from '@material-ui/core';
 
 export const CardItem = styled.li`
   cursor: pointer;
-  /* padding: 10px; */
   box-shadow: 1px 1px 8px 1px #ccc;
   border-radius: 4px;
   transition: all .2s ease;
